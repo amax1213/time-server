e6f3a90dac5bd0b2b4e3ff59100e74ad56e2525a
allen maxwell <allen.maxwell@outlook.com>
2021-06-24 17:54:24 -0700

    Update time-server.py
    Resolve address issue

--- time-server.py
+++ time-server.py
@@ -1,3 +1,4 @@
+from flask.wrappers import Response
 import requests
 import os
 from flask import Flask
@@ -6,19 +7,14 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 response = requests.get("https://www.epochconverter.com/")
-
 etime = {}
 soup = BeautifulSoup(response.content, "html.parser")
 etime = soup.find_all("div", class_="ecclock")
 
-print(etime[0].getText())
-
-
-@app.route("/")
-def get_epoch_time(): #home
-    print(etime[0].getText())
+@app.route('/')
+def get_time():
     return str(etime[0].getText())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6783))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host="127.0.0.1", port=port)
