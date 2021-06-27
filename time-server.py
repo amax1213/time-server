39568a049ce39c7ad7c6f3eef1c0513b35bcd250
allen maxwell <allen.maxwell@outlook.com>
2021-06-27 13:39:32 -0700

    Initial commit

--- time-server.py
+++ time-server.py
@@ -13,8 +13,8 @@ def get_epoch_time():
 @app.route('/')
 def home():
     etime = get_epoch_time()
-    return str(etime[0].getText())
+    return etime[0].getText()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host="127.0.0.1", port=port)
+    app.run(host="0.0.0.0", port=port)
