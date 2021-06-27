6d70e92a3b09bd241a55acbc1256752be06117e0
allen maxwell <allen.maxwell@outlook.com>
2021-06-27 12:57:29 -0700

    Initial commit

--- time-server.py
+++ time-server.py
@@ -5,16 +5,16 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-response = requests.get("https://www.epochconverter.com/")
-etime = {}
-soup = BeautifulSoup(response.content, "html.parser")
-etime = soup.find_all("div", class_="ecclock")
+def get_epoch_time():
+    response = requests.get("https://www.epochconverter.com/")
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup.find_all("div", class_="ecclock")
 
 @app.route('/')
-def get_time():
+def home():
+    etime = get_epoch_time()
     return str(etime[0].getText())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host="127.0.0.1", port=port)
-
